f001e611a23178b255a9d682f26a57d25ee543c0
Ana Bebic <anabebic.x@gmail.com>
2018-07-07 12:52:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,17 @@ class Song
 end
 
 def self.genres_count
-    @@genres.uniq
+  
+    genre_count = {}	
+  @@genres.each do |genre|	
+      if genre_count[genre]	
+       genre_count[genre] += 1 	
+      else	
+        genre_count[genre] = 1	
+      end	
+    end	
+    genre_count	
+  end
 
 end
 end
