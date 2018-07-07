89b9fbed3c521bc53363302c5dd28e7bb07fa69f
Ana Bebic <anabebic.x@gmail.com>
2018-07-07 12:49:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,22 @@
 class Song	
--  attr_accessor :name, :artist, :genre	
--	
--  @@count = 0	
--  @@artists = []	
--  @@genres = []	
--	
--  def initialize(name, artist, genre)	
--    @name = name	
--    @artist = artist	
--    @genre = genre	
--    @@count +=1	
--    @@genres << genre	
--    @@artists << artist	
--  end	
--	
--  def self.count	
--    @@count	
--  end	
+   attr_accessor :name, :artist, :genre	
+
+  @@count = 0	
+  @@artists = []	
+   @@genres = []	
+
+  def initialize(name, artist, genre)	
+    @name = name	
+    @artist = artist	
+    @genre = genre	
+    @@count +=1	
+    @@genres << genre	
+    @@artists << artist	
+  end	
+	
+  def self.count	
+    @@count	
+  end	
 -	
 -  def self.artists	
 -    @@artists.uniq	
